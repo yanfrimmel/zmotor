6e17848ba046a5365d35ce2098df7507a034c05c
Yan Frimmel <yanfrimmel1411@gmail.com>
2023-12-19 04:28:33 +0000

    Refactored engine

--- src/engine.zig
+++ src/engine.zig
@@ -7,46 +7,51 @@ const assert = @import("std").debug.assert;
 const common = @import("common.zig");
 
 pub fn start(width: u16, height: u16, logic: *const fn (input: ?common.InputEvent) common.GraphicalGameState) !void {
-    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO | sdl.SDL_INIT_EVENTS) < 0) {
-        sdl.SDL_Log("Unable to initialize SDL: %s", sdl.SDL_GetError());
-        return error.SDLInitializationFailed;
-    }
+    try initSdl();
     defer sdl.SDL_Quit();
-
-    if (sdl.IMG_Init(sdl.IMG_INIT_PNG) < 0) {
-        sdl.SDL_Log("Unable to initialize SDL Image: %s", sdl.SDL_GetError());
-        return error.SDLInitializationFailed;
-    }
     defer sdl.IMG_Quit();
 
-    const screen = sdl.SDL_CreateWindow("My Game Window", sdl.SDL_WINDOWPOS_UNDEFINED, sdl.SDL_WINDOWPOS_UNDEFINED, width, height, sdl.SDL_WINDOW_OPENGL) orelse
+    const window = try initWindow(width, height);
+    defer sdl.SDL_DestroyWindow(window);
+
+    const renderer = try initRenderer(window);
+    defer sdl.SDL_DestroyRenderer(renderer);
+
+    try gameLoog(renderer, logic);
+}
+
+fn initRenderer(window: *sdl.SDL_Window) !*sdl.SDL_Renderer {
+    const renderer = sdl.SDL_CreateRenderer(window, -1, 0) orelse {
+        sdl.SDL_Log("Unable to create renderer: %s", sdl.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    return renderer;
+}
+
+fn initWindow(width: u16, height: u16) !*sdl.SDL_Window {
+    const window = sdl.SDL_CreateWindow("My Game Window", sdl.SDL_WINDOWPOS_UNDEFINED, sdl.SDL_WINDOWPOS_UNDEFINED, width, height, sdl.SDL_WINDOW_OPENGL) orelse
         {
         sdl.SDL_Log("Unable to create window: %s", sdl.SDL_GetError());
         return error.SDLInitializationFailed;
     };
-    defer sdl.SDL_DestroyWindow(screen);
+    return window;
+}
 
-    const renderer = sdl.SDL_CreateRenderer(screen, -1, 0) orelse {
-        sdl.SDL_Log("Unable to create renderer: %s", sdl.SDL_GetError());
+fn initSdl() !void {
+    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO | sdl.SDL_INIT_EVENTS) < 0) {
+        sdl.SDL_Log("Unable to initialize SDL: %s", sdl.SDL_GetError());
         return error.SDLInitializationFailed;
-    };
-    defer sdl.SDL_DestroyRenderer(renderer);
+    }
 
-    const example = @embedFile("../assets/dirt.png");
-    const rw = sdl.SDL_RWFromConstMem(example, example.len) orelse {
-        sdl.SDL_Log("Unable to get RWFromConstMem: %s", sdl.SDL_GetError());
+    if ((sdl.IMG_Init(sdl.IMG_INIT_PNG) & sdl.IMG_INIT_PNG) == 0) {
+        sdl.SDL_Log("Unable to initialize SDL Image: %s", sdl.IMG_GetError());
         return error.SDLInitializationFailed;
-    };
-    defer assert(sdl.SDL_RWclose(rw) == 0);
+    }
+}
 
-    const exampleSurface = sdl.IMG_LoadPNG_RW(rw) orelse {
-        sdl.SDL_Log("Unable to load bmp: %s", sdl.SDL_GetError());
-        return error.SDLInitializationFailed;
-    };
-    defer sdl.SDL_FreeSurface(exampleSurface);
-
-    const exampleTexture = sdl.SDL_CreateTextureFromSurface(renderer, exampleSurface) orelse {
-        sdl.SDL_Log("Unable to create texture from surface: %s", sdl.SDL_GetError());
+fn gameLoog(renderer: *sdl.SDL_Renderer, logic: *const fn (input: ?common.InputEvent) common.GraphicalGameState) !void {
+    const exampleTexture = sdl.IMG_LoadTexture(renderer, "assets/dirt.png") orelse {
+        sdl.SDL_Log("Unable to load textutre: %s", sdl.IMG_GetError());
         return error.SDLInitializationFailed;
     };
     defer sdl.SDL_DestroyTexture(exampleTexture);
@@ -62,8 +67,7 @@ pub fn start(width: u16, height: u16, logic: *const fn (input: ?common.InputEven
                 else => {},
             }
         }
-        const gameState = logic(null);
-        _ = gameState;
+        try draw(logic(null));
         // TODO: draw state
 
         _ = sdl.SDL_RenderClear(renderer);
